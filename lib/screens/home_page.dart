eeee2c295d43d108a1b8472199cc5f4f4c9d2888
nevilpurpp <nevilpurpp12@gmail.com>
2024-01-27 10:37:37 +0300

    updates

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 import '../models/api.dart'; // Import your API class
@@ -51,7 +52,7 @@ class _HomePageState extends State<HomePage> {
                   width: 300, 
                      child: SfCartesianChart(
                     tooltipBehavior: _tooltipBehavior,
-                    title: ChartTitle(text: 'Hydroponic Sensor Data'),
+                    title: ChartTitle(text: 'Sensor Data'),
                     legend: Legend(isVisible: true),
                     primaryXAxis: CategoryAxis(),
                     palette: <Color>[
@@ -59,6 +60,7 @@ class _HomePageState extends State<HomePage> {
                       Colors.red.withOpacity(0.5),
                       Colors.green.withOpacity(0.5),
                       Colors.yellow.withOpacity(0.5),
+                      Colors.purple.withOpacity(0.5)
                     ],
                     series: <CartesianSeries<Sensor, String>>[
                       ColumnSeries<Sensor, String>(
@@ -93,10 +95,75 @@ class _HomePageState extends State<HomePage> {
                         dataLabelSettings: DataLabelSettings(isVisible: false),
                         enableTooltip: true,
                       ),
+                      ColumnSeries<Sensor, String>(
+                        name: "Nutrients",
+                        dataSource: sensorData,
+                        xValueMapper: (Sensor data, _) => data.sensorName,
+                        yValueMapper: (Sensor data, _) => data.waterlvl,
+                        dataLabelSettings: DataLabelSettings(isVisible: false),
+                        enableTooltip: true,
+                      ),
                     ],
                   ),
                 ),
-                
+                StaggeredGrid.count(
+  crossAxisCount: 4,
+  mainAxisSpacing: 4,
+  crossAxisSpacing: 4,
+  children:   [
+    StaggeredGridTile.count(
+      crossAxisCellCount: 2,
+      mainAxisCellCount: 2,
+      child: Card(
+        child: Row(children: [
+          Image.asset('asset/logos/temperature.png'),
+          const Text('27 ℃', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold))
+        ],)
+      )
+    ),
+    StaggeredGridTile.count(
+      crossAxisCellCount: 2,
+      mainAxisCellCount: 1,
+      child: Card(
+        child: Row(children: [
+          Image.asset('asset/logos/water.png'),
+          const Text('18 ⁜', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold))
+        ],)
+      )
+    ),
+    StaggeredGridTile.count(
+      crossAxisCellCount: 1,
+      mainAxisCellCount: 1,
+      child: Card(
+        child: Row(children: [
+          Expanded(child: Image.asset('asset/logos/ph.png', )),
+          Expanded(child: const Text('7',style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)))
+        ],)
+      ),
+    ),
+    StaggeredGridTile.count(
+      crossAxisCellCount: 1,
+      mainAxisCellCount: 1,
+      child: Card(
+        child: Row(children: [
+          Image.asset('asset/logos/humidity.png'),
+          //const Text('Humidity')
+        ],)
+      )
+    ),
+    StaggeredGridTile.count(
+      crossAxisCellCount: 4,
+      mainAxisCellCount: 2,
+      child: Card(
+        child: Row(children: [
+          Image.asset('asset/logos/nutrient.png'),
+          //const Text('Nutrients')
+        ],)
+      ),
+    ),
+  ],
+)
+
                 
                 ]
               );
