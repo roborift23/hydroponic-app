c5a25ebabf39b7a4672672c1eb5c457552561ca4
nevilpurpp <nevilpurpp12@gmail.com>
2024-01-30 21:02:15 +0300

    sensor output data updates

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-
 import '../models/api.dart'; // Import your API class
 import '../models/sensor_data.dart'; // Import your Sensor class
 
@@ -12,12 +10,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late Stream<List<Sensor>> sensorDataStream;
-  late TooltipBehavior _tooltipBehavior;
+  
 
   @override
   void initState() {
     super.initState();
-    _tooltipBehavior = TooltipBehavior(enable: true);
+    
     sensorDataStream = GetRequest().getSensorDataStream();
   }
 
@@ -47,65 +45,7 @@ class _HomePageState extends State<HomePage> {
 
               return ListView(
               shrinkWrap: true,
-                children:[ SizedBox(
-                  height: 400,
-                  width: 300, 
-                     child: SfCartesianChart(
-                    tooltipBehavior: _tooltipBehavior,
-                    title: ChartTitle(text: 'Sensor Data'),
-                    legend: Legend(isVisible: true),
-                    primaryXAxis: CategoryAxis(),
-                    palette: <Color>[
-                      Colors.blue.withOpacity(0.5),
-                      Colors.red.withOpacity(0.5),
-                      Colors.green.withOpacity(0.5),
-                      Colors.yellow.withOpacity(0.5),
-                      Colors.purple.withOpacity(0.5)
-                    ],
-                    series: <CartesianSeries<Sensor, String>>[
-                      ColumnSeries<Sensor, String>(
-                        name: "pH",
-                        dataSource: sensorData,
-                        xValueMapper: (Sensor data, _) => data.sensorName,
-                        yValueMapper: (Sensor data, _) => data.ph,
-                        dataLabelSettings: DataLabelSettings(isVisible: false),
-                        enableTooltip: true,
-                      ),
-                      ColumnSeries<Sensor, String>(
-                        name: "Temperature",
-                        dataSource: sensorData,
-                        xValueMapper: (Sensor data, _) => data.sensorName,
-                        yValueMapper: (Sensor data, _) => data.temperature,
-                        dataLabelSettings: DataLabelSettings(isVisible: false),
-                        enableTooltip: true,
-                      ),
-                      ColumnSeries<Sensor, String>(
-                        name: "Humidity",
-                        dataSource: sensorData,
-                        xValueMapper: (Sensor data, _) => data.sensorName,
-                        yValueMapper: (Sensor data, _) => data.humidity,
-                        dataLabelSettings: DataLabelSettings(isVisible: false),
-                        enableTooltip: true,
-                      ),
-                      ColumnSeries<Sensor, String>(
-                        name: "Water Level",
-                        dataSource: sensorData,
-                        xValueMapper: (Sensor data, _) => data.sensorName,
-                        yValueMapper: (Sensor data, _) => data.waterlvl,
-                        dataLabelSettings: DataLabelSettings(isVisible: false),
-                        enableTooltip: true,
-                      ),
-                      ColumnSeries<Sensor, String>(
-                        name: "Nutrients",
-                        dataSource: sensorData,
-                        xValueMapper: (Sensor data, _) => data.sensorName,
-                        yValueMapper: (Sensor data, _) => data.waterlvl,
-                        dataLabelSettings: DataLabelSettings(isVisible: false),
-                        enableTooltip: true,
-                      ),
-                    ],
-                  ),
-                ),
+                children:[ 
                 StaggeredGrid.count(
   crossAxisCount: 4,
   mainAxisSpacing: 4,
@@ -117,7 +57,7 @@ class _HomePageState extends State<HomePage> {
       child: Card(
         child: Row(children: [
           Image.asset('asset/logos/temperature.png'),
-          const Text('27 ℃', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold))
+           Text(sensorData.isNotEmpty ? sensorData[0].temperature.toString() : 'N/A', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold))
         ],)
       )
     ),
@@ -127,7 +67,7 @@ class _HomePageState extends State<HomePage> {
       child: Card(
         child: Row(children: [
           Image.asset('asset/logos/water.png'),
-          const Text('18 ⁜', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold))
+         Text(sensorData.isNotEmpty ? sensorData[0].waterlvl.toString() : 'N/A', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold))
         ],)
       )
     ),
@@ -137,7 +77,7 @@ class _HomePageState extends State<HomePage> {
       child: Card(
         child: Row(children: [
           Expanded(child: Image.asset('asset/logos/ph.png', )),
-          Expanded(child: const Text('7',style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)))
+           Expanded(child: Text(sensorData.isNotEmpty ? sensorData[0].ph.toString() : 'N/A', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)))
         ],)
       ),
     ),
